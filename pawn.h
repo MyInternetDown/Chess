85d20ba5b97060daf5bfb81ebad6a5197e503322
Hubert <qinhubert@gmail.com>
2023-11-28 19:23:42 -0500

    pawn

---
+++ pawn.h
@@ -0,0 +1,15 @@
+#ifndef __Bishop__
+#define __Pawn__
+#include "chesspiece.h"
+#include <string>
+
+class Pawn : public ChessPiece {
+public:
+    Pawn(vector<int> pos, bool Black);
+    string getName();
+    bool isBlocked(vector<int> pos);
+    bool promote(vector<int> pos);
+    bool enPassant(vector<int>pos, vector<vector<int>>prevMove);
+};
+
+#endif
