3c54404a628b8cfe74159f2ffb550ecce79b9742
Richard Zhao <richardjzzhao@gmail.com>
2023-11-28 19:21:08 -0500

    changed bishop.h

--- bishop.h
+++ bishop.h
@@ -6,6 +6,7 @@
 class Bishop : public ChessPiece {
 public:
     Bishop(vector<int> pos, bool Black);
+    ~Bishop();
     bool canMove(const vector<int> position, const vector<int> destination, const vector<vector<ChessPiece>>) const override;
     char Type() const override;
 };
