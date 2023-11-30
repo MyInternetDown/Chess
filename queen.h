70b9c19ee13b1e6ce64888d5691a6894ca30cc9a
Robin Zhang <robin.zhang0403@gmail.com>
2023-11-30 16:25:06 -0500

    stuff

--- queen.h
+++ queen.h
@@ -11,7 +11,7 @@ public:
     Queen(string pos, string white);
 
     // Override the getAllMoves function for the Queen
-    vector<Coordinate> getAllMoves(const Coordinate position, const vector<vector<ChessPiece>> &board) override;
+    vector<Coordinate> getAllMoves(const Coordinate position, const vector<vector<ChessPiece>> &board) const override;
 };
 
 #endif
