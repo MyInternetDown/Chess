0554f787863d4570c4eec6e0406670cc1ff1ee11
Robin Zhang <robin.zhang0403@gmail.com>
2023-12-04 03:22:36 -0500

    main works

--- bishop.d
+++
@@ -1,1 +0,0 @@
-bishop.o: bishop.cc bishop.h chesspiece.h enum.h coordinate.h observer.h
--- main.d
+++
@@ -1,3 +0,0 @@
-main.o: main.cc board.h player.h textdisplay.h observer.h \
- graphicsdisplay.h window.h coordinate.h king.h chesspiece.h enum.h \
- queen.h bishop.h knight.h rook.h pawn.h
