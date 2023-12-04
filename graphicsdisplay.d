e2139809e11b1d93ab292057e3b0034f08afca80
Robin Zhang <robin.zhang0403@gmail.com>
2023-12-03 19:17:06 -0500

    board T1 and human moves

--- graphicsdisplay.d
+++
@@ -1,3 +0,0 @@
-graphicsdisplay.o: graphicsdisplay.cc graphicsdisplay.h observer.h \
- window.h board.h player.h textdisplay.h coordinate.h king.h chesspiece.h \
- enum.h queen.h bishop.h knight.h rook.h pawn.h
